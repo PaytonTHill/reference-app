7d201ed2da45ff8e77dd2fd9db220a81923f82ad
Payton <payton.slim@gmail.com>
2023-06-05 09:28:12 -0700

    finished?

--- src/components/ReferenceList.js
+++ src/components/ReferenceList.js
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import AddReference from './AddReference';
-import './App.css'
+import './App.css';
 
 function ReferenceList() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [content, setContent] = useState('');
   const [references, setReferences] = useState([]);
 
   function fetchReferences() {
@@ -20,77 +17,16 @@ function ReferenceList() {
       });
   }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (name && email && content) {
-      const newReference = {
-        name,
-        email,
-        reference_content: content,
-      };
-      fetch('/api/addReference', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newReference),
-      })
-        .then(response => response.json())
-        .then(result => {
-          console.log('Reference added successfully:', result);
-          setName('');
-          setEmail('');
-          setContent('');
-        })
-        .catch(error => {
-          console.error('Error adding reference:', error);
-        });
-    }
-  }
-
   useEffect(() => {
     fetchReferences();
   }, []);
 
   return (
     <div>
-        <AddReference />
-        <form onSubmit={handleSubmit}>
-            <h2>Add Reference</h2>
-            <div>
-            <label htmlFor="name">Name:</label>
-            <input
-                type="text"
-                id="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-            />
-            </div>
-            <div>
-            <label htmlFor="email">Email:</label>
-            <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-            />
-            </div>
-            <div>
-            <label htmlFor="content">Reference:</label>
-            <textarea
-                id="content"
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
-                required
-            ></textarea>
-            </div>
-            <button type="submit">Submit</button>
-        </form>
+      <AddReference />
 
       <div id="reference-list">
-        {references.map((reference) => (
+        {references.map(reference => (
           <div key={reference.id} className="reference">
             <div className="reference-header">
               <h3>{reference.name}</h3>
